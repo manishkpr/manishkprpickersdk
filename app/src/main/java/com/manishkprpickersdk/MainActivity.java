33ea9bd03355b11c5ef7336e06c4eda7dc5351a8
Edge <munish.kapoor@bizedge.com>
2017-04-13 13:53:27 +0800

    build 1 version 1.0

--- app/src/main/java/com/manishkprpickersdk/MainActivity.java
+++ app/src/main/java/com/manishkprpickersdk/MainActivity.java
@@ -1,13 +1,224 @@
+/*
+ * Copyright (c) 2016. Ted Park. All Rights Reserved
+ */
+
 package com.manishkprpickersdk;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.manishkprpickersdkit.Config;
+import com.manishkprpickersdkit.ImagePickerActivity;
+
+
+import java.util.ArrayList;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
+
+    private static final int INTENT_REQUEST_GET_IMAGES = 13;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    ArrayList<Uri> image_uris = new ArrayList<Uri>();
+    private ViewGroup mSelectedImagesContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mSelectedImagesContainer = (ViewGroup) findViewById(R.id.selected_photos_container);
+        View getImages = findViewById(R.id.get_images);
+        getImages.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                getImages(new Config());
+            }
+        });
+
+
+        View getImages2 = findViewById(R.id.get_images2);
+        getImages2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Config config = new Config();
+                config.setCameraHeight(R.dimen.app_camera_height);
+                config.setToolbarTitleRes(R.string.custom_title);
+                config.setSelectionMin(2);
+                config.setSelectionLimit(4);
+                config.setSelectedBottomHeight(R.dimen.bottom_height);
+                config.setFlashOn(true);
+
+
+                getImages(config);
+            }
+        });
+
+        MainActivityPermissionsDispatcher.showCameraWithCheck(MainActivity.this);
     }
+
+    private void getImages(Config config) {
+
+
+        ImagePickerActivity.setConfig(config);
+
+        Intent intent = new Intent(this, ImagePickerActivity.class);
+
+        if (image_uris != null) {
+            intent.putParcelableArrayListExtra(ImagePickerActivity.EXTRA_IMAGE_URIS, image_uris);
+        }
+
+
+        startActivityForResult(intent, INTENT_REQUEST_GET_IMAGES);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == INTENT_REQUEST_GET_IMAGES) {
+
+                image_uris = intent.getParcelableArrayListExtra(ImagePickerActivity.EXTRA_IMAGE_URIS);
+
+                if (image_uris != null) {
+                    showMedia();
+                }
+
+
+            }
+        }
+    }
+
+
+    private void showMedia() {
+        // Remove all views before
+        // adding the new ones.
+        mSelectedImagesContainer.removeAllViews();
+        if (image_uris.size() >= 1) {
+            mSelectedImagesContainer.setVisibility(View.VISIBLE);
+        }
+
+        int wdpx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics());
+        int htpx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics());
+
+
+        for (Uri uri : image_uris) {
+
+            View imageHolder = LayoutInflater.from(this).inflate(R.layout.image_item, null);
+            ImageView thumbnail = (ImageView) imageHolder.findViewById(R.id.media_image);
+
+            Glide.with(this)
+                    .load(uri.toString())
+                    .fitCenter()
+                    .into(thumbnail);
+
+            mSelectedImagesContainer.addView(imageHolder);
+
+            thumbnail.setLayoutParams(new FrameLayout.LayoutParams(wdpx, htpx));
+
+
+        }
+
+    }
+
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    void showCamera() {
+        MainActivityPermissionsDispatcher.readStorageWithCheck(this);
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    void showRationaleForCamera(PermissionRequest request) {
+        // NOTE: Show a rationale to explain why the permission is needed, e.g. with a dialog.
+        // Call proceed() or cancel() on the provided PermissionRequest to continue or abort
+        showRationaleDialog(R.string.permission_camera_rationale, request);
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void onCameraDenied() {
+        // NOTE: Deal with a denied permission, e.g. by showing specific UI
+        // or disabling certain functionality
+    }
+
+
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void readStorage() {
+
+    }
+
+    @OnShowRationale(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showRationaleForReadStorage(PermissionRequest request) {
+        // NOTE: Show a rationale to explain why the permission is needed, e.g. with a dialog.
+        // Call proceed() or cancel() on the provided PermissionRequest to continue or abort
+        showRationaleDialog(R.string.permission_storage_rationale, request);
+    }
+
+    @OnPermissionDenied(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void onReadStorageDenied() {
+        // NOTE: Deal with a denied permission, e.g. by showing specific UI
+        // or disabling certain functionality
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    private void showRationaleDialog(@StringRes int messageResId, final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setPositiveButton(R.string.button_allow, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton(R.string.button_deny, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .setMessage(messageResId)
+                .show();
+    }
+
+
 }
